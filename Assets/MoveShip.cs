041f25cb7e3d1639f60e2bef572a0a9de542440a
Richard Leinfellner <richard@modulo17.com>
2017-02-07 11:56:28 +0000

    First Upload, basic move code for ship

---
+++ Assets/MoveShip.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveShip : MonoBehaviour
+{
+    float mSpeed=0.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            transform.Rotate(0, 0, 360f * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))  {
+            transform.Rotate(0, 0, -360f * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.UpArrow))  {
+            mSpeed += 0.1f;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            mSpeed -= 0.1f;
+        }
+        transform.position += transform.rotation*Vector3.up*Time.deltaTime*mSpeed;
+    }
+}
---
+++ Assets/Wrap.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wrap : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float tHeight = Camera.main.orthographicSize;       //Height
+        float tWidth = tHeight * Camera.main.aspect;
+        if (transform.position.y > tHeight)  {
+            transform.position += Vector3.down * tHeight * 2f;
+        }
+        if (transform.position.y < -tHeight) {
+            transform.position += Vector3.up * tHeight * 2f;
+        }
+
+        if (transform.position.x > tWidth) {
+            transform.position += Vector3.left * tWidth * 2f;
+        }
+
+        if (transform.position.x <- tWidth) {
+            transform.position += Vector3.right * tWidth * 2f;
+        }
+
+    }
+}
