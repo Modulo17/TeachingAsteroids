e4d6c138558f26ef5b0b376ad42c99de08c4c3c7
rleinfellner <richard@modulo17.com>
2017-02-14 11:49:05 +0000

    Updated Physics module

--- Assets/PhysicsShip.cs
+++ Assets/PhysicsShip.cs
@@ -4,8 +4,11 @@ using System.Collections;
 public class PhysicsShip : MonoBehaviour {
 
     [Header("User settings")]           //These will show up in inspector
-    public float RotationSpeed = 360f;
-    public float Speed = 100.0f;
+
+	[Range(0,100f)]
+    public float RotationSpeed = 2;
+	[Range(0,100f)]
+    public float Speed = 10.0f;
 
     Rigidbody2D mRB;  //Keep a reference to the RB
 	void Start () {
@@ -17,13 +20,13 @@ public class PhysicsShip : MonoBehaviour {
     void FixedUpdate()
     {
             if (Input.GetKey(KeyCode.LeftArrow)) {      //Rotate ship, coudl use torque, but this looks better
-             mRB.rotation+=Time.deltaTime*RotationSpeed;
+				mRB.AddTorque(RotationSpeed*Time.deltaTime);
             }
             if (Input.GetKey(KeyCode.RightArrow)) {
-            mRB.rotation -= Time.deltaTime*RotationSpeed;
-        }
+			mRB.AddTorque(-RotationSpeed*Time.deltaTime);
+    	    }
         if (Input.GetKey(KeyCode.UpArrow)) {    //Apply force in direction of rotation
-            Vector2 tForce = Quaternion.Euler(0,0, mRB.rotation)* Vector2.up * Time.deltaTime * Speed;
+			Vector2 tForce = Quaternion.Euler(0,0, mRB.rotation)* Vector2.up * Time.deltaTime*  Speed;
             mRB.AddForce(tForce);
         }
     }
